e264e2879a9b8877038b6a6b38653c7171b547dd
Bairy-abhilash <abhilashmbairy@gmail.com>
2026-07-06 16:36:53 +0530

    port change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import {PORT} from './config/env.js';
 const app = express();
 
 //first route
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 //listen for requests
-app.listen(3000, () => {
-    console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT, 'localhost', () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 });
 export default app;
