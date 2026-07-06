0c50c5a591a9e2ed1bd29195803bdd458ab81754
Bairy-abhilash <abhilashmbairy@gmail.com>
2026-07-06 23:32:03 +0530

    added routes

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ import subscriptionRouter from './routes/subscription.routes.js';
 
 const app = express();
 
-app.use('api/v1/auth', authRouter);
-app.use('api/v1/users', userRouter);
-app.use('api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 //first route
 app.get('/', (req, res) => {
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 //listen for requests
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 });
 export default app;
