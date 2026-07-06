ba9a558d41717471e3262b5e9908296011c9116b
Bairy-abhilash <abhilashmbairy@gmail.com>
2026-07-06 23:08:09 +0530

    modified subscription route

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -6,5 +6,8 @@ subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET Subscription
 subscriptionRouter.post('/', (req, res) => res.send({ title: 'Create new Subscription' }));
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'Update Subscription ' }));
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'Delete Subscription ' }));
+subscriptionRouter.get('/user/:id', (req, res) => res.send({title: 'GET all user Subscriptions '}));
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: 'Cancel user Subscription '}));
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({title: 'GET upcoming renewals '}));
 
 export default subscriptionRouter;
